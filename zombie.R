00e28468b138a3b704491f7110432f9a6ca767dd
Marcio T <mteix@users.noreply.github.com>
2015-03-05 08:39:53 -0300

    Update zombie.R

--- zombie.R
+++ zombie.R
@@ -137,59 +137,6 @@ for (k in 1:iter) {
       }
 
 
-
-#       ##############################
-#       # And they may walk
-#       ##############################
-#   
-#       #choose a direction
-#       
-#       theWalk <- floor(runif(2, min=-1, max=2))
-#       iWalk <- theWalk[1]+i
-#       jWalk <- theWalk[2]+j
-#       
-#       #again the boundary conditions
-#       
-#       if(iWalk > m) iWalk <- 1
-#       if(iWalk < 1) iWalk <- m
-#       if(jWalk > m) jWalk <- 1
-#       if(jWalk < 1) jWalk <- m
-#       
-#       #### The walking dead
-#       
-#       if(theLife[i,j]==1){
-#         
-#         #an empty space to walk
-#         if (theLife[iWalk,jWalk]==0){
-#           theLife[i,j] <- 0
-#           theLife[iWalk,jWalk] <- 1
-#         }  
-#         
-#       }
-#       
-#       #### The walking humans
-#       
-#       if(theLife[i,j]==2){
-#         
-#         #an empty space to walk
-#         if (theLife[iWalk,jWalk]==0){
-#           theLife[i,j] <- 0
-#           theLife[iWalk,jWalk] <- 2
-#         }  
-# #        #don't hit a zombie or...
-# #         if (theLife[iWalk,jWalk]==1){
-# #           theLife[i,j] <- 1
-# #         }  
-#           #############################
-#           # end walk
-#           ##############################
-#           #  
-#       }
-#       
-      #############################
-       #END OF RULES
-      ############################
-
     } ###Loop j
     
   } ###Loop i
@@ -200,7 +147,8 @@ for (k in 1:iter) {
 
 } ###Loop Step
 
-Sys.time()-startTime
+cat("\n\n\")
+print(Sys.time()-startTime)
 
 startTime <- Sys.time()
 write.gif(X, "zombie.gif",col=jet.colors, delay=50)
